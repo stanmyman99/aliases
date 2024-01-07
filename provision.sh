55e17f0f95035a3eecf4ebc4ab742329b734031c
stanmyman99 <97717108+stanmyman99@users.noreply.github.com>
2024-01-07 11:24:24 -0700

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -6,12 +6,6 @@ cd ~/Code
 git clone https://github.com/stanmyman99/aliases
 popd
 
-# Create the provisioning directory and download Veracrypt
-#
-pushd .
-mkdir ~/Apps/veracrypt
-cd ~/Apps/veracrypt
-wget https://www.veracrypt.fr/code/VeraCrypt/snapshot/VeraCrypt_1.26.7.tar.gz
 
 
 
