cb32a2130dbe4c2b7bf63da1d61dbed0609f5f53
stanmyman99 <97717108+stanmyman99@users.noreply.github.com>
2024-01-07 11:23:01 -0700

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,8 +1,17 @@
-#   # Aliases pre-configured
+#  Download the aliases and command references
 # 
 pushd .
 mkdir ~/Code
 cd ~/Code
 git clone https://github.com/stanmyman99/aliases
+popd
+
+# Create the provisioning directory and download Veracrypt
+#
+pushd .
+mkdir ~/Apps/veracrypt
+cd ~/Apps/veracrypt
+wget https://www.veracrypt.fr/code/VeraCrypt/snapshot/VeraCrypt_1.26.7.tar.gz
+
 
 
