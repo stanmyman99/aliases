bb33cc96c6854038fbd7885efa65401d13fdb4e9
stanmyman99 <97717108+stanmyman99@users.noreply.github.com>
2024-01-07 10:58:39 -0700

    Update commands.sh
    Android emulator

--- commands.sh
+++ commands.sh
@@ -12,5 +12,28 @@ sudo make dkms_install
 #
 nmcli dev wifi
 
+# ---------------------------------------------------------------
+# Android emulator commands
+#  See the android-emulator-options.sh script for these in a menu
+# ---------------------------------------------------------------
+pushd ~/Android/Sdk/emulator
+./emulator -list-avds
+
+# Boot the device, save changes
+#
+./emulator -avd Pixel_6_Pro_API_33
+
+# Boot the devide from off state and save changes
+# 
+./emulator -avd Pixel_6_Pro_API_33 -no-snapshot-load
+
+# Boot the device but don't save changes
+#
+./emulator -avd Pixel_6_Pro_API_33 -no-snapshot-save
+
+# Reset the device to factory settings
+#
+./emulator -avd Pixel_6_Pro_API_33 -wipe-data
+
 
 
