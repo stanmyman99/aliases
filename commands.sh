fca0623c530d784c506079e715bef9b20fd73813
stanmyman99 <97717108+stanmyman99@users.noreply.github.com>
2024-01-07 11:14:36 -0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,20 +1,49 @@
-# Build the driver for the TP-Link AC600
-#
-sudo apt install git dkms
-git clone https://github.com/aircrack-ng/rtl8812au.git
-cd rtl8812au
-sudo make dkms_install
-#The following commands are only needed if there was already a driver present and you're updating
-# sudo mv /var/lib/8812au /var/lib/8812au-orig
-# sudo make dkms_install
+# ---------------------------------------------------------------
+# Veracrypt reference
+# ---------------------------------------------------------------
 
-# Show available wifi
+# Building veracrypt
 #
-nmcli dev wifi
+pushd ~
+mkdir ~/Apps/veracrypt
+cd ~/Apps/veracrypt
+wget https://www.veracrypt.fr/code/VeraCrypt/snapshot/VeraCrypt_1.26.7.tar.gz
+sudo apt install g++ libfuse-dev pkg-config yasm libwxbase3.0-dev
+tar -xvf VeraCrypt-VeraCrypt_1.26.7.tar.gz
+cd VeraCrypt-VeraCrypt_1.26.7/src
+make NOGUI=1
+sudo cp Main/veracrypt /usr/bin
+
+# Create a new volume:
+#
+veracrypt -t -c
+
+# Mount a volume:
+#
+veracrypt volume.hc /media/veracrypt1
+
+# Mount a volume as read-only, using keyfiles:
+#
+veracrypt -m ro -k keyfile1,keyfile2 volume.hc
+
+# Mount a volume without mounting its filesystem:
+#
+veracrypt --filesystem=none volume.hc
+
+# Mount a volume prompting only for its password:
+#
+veracrypt -t -k "" --pim=0 --protect-hidden=no volume.hc /media/veracrypt1
+
+# Dismount a volume:
+#
+veracrypt -d volume.hc
+
+# Dismount all mounted volumes:
+#
+veracrypt -d
 
 # ---------------------------------------------------------------
 # Android emulator commands
-#  See the android-emulator-options.sh script for these in a menu
 # ---------------------------------------------------------------
 pushd ~/Android/Sdk/emulator
 ./emulator -list-avds
@@ -36,4 +65,39 @@ pushd ~/Android/Sdk/emulator
 ./emulator -avd Pixel_6_Pro_API_33 -wipe-data
 
 
+# Aliases pre-configured
+# 
+git clone https://github.com/stanmyman99/aliases.git
+
+# Veracrypt container creation
+#
+veracrypt -t -c ~/Documents/Encrypted.vc
+
+# Veracrypt mounting
+#
+veracrypt ~/Documents/Encrypted.vc /media/veracrypt1
+
+# Veracrypt soft linking
+#
+pushd ~/Documents
+ln -s /media/veracrypt1 Encrypted
+popd
+
+# ---------------------------------------------------------------
+# Kali linux commands
+# ---------------------------------------------------------------
+
+# Build the driver for the TP-Link AC600
+#
+sudo apt install git dkms
+git clone https://github.com/aircrack-ng/rtl8812au.git
+cd rtl8812au
+sudo make dkms_install
+#The following commands are only needed if there was already a driver present and you're updating
+# sudo mv /var/lib/8812au /var/lib/8812au-orig
+# sudo make dkms_install
+
+# Show available wifi
+#
+nmcli dev wifi
 
