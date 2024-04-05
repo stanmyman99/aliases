1b39ecd0e4be51ae78d92b012eb90fe92a27de91
stanmyman99 <97717108+stanmyman99@users.noreply.github.com>
2024-04-05 13:34:13 -0600

    Update mobile-baseline.sh

--- mobile-baseline.sh
+++ mobile-baseline.sh
@@ -5,5 +5,4 @@
 
 wget --no-check-certificate --content-disposition https://github.com/ImranR98/Obtainium/releases/download/v1.1.1/app-arm64-v8a-release.apk
 wget --no-check-certificate --content-disposition https://github.com/ImranR98/Obtainium/releases/download/v1.1.1/app-arm64-v8a-release.apk.sha256
-sha256sum -c app-arm64-v8a-release.apk.sha256
 
