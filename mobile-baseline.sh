5877bbf6dc1862a46a9ff17d9d5343939c731a23
stanmyman99 <97717108+stanmyman99@users.noreply.github.com>
2024-04-05 13:14:54 -0600

    Update mobile-baseline.sh

--- mobile-baseline.sh
+++ mobile-baseline.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
+# Downloads android FOSS mobile apps from the sources to provision a new phone
 #
 
-curl
+wget --no-check-certificate --content-disposition https://github.com/ImranR98/Obtainium/releases/download/v1.1.1/app-arm64-v8a-release.apk
+wget --no-check-certificate --content-disposition https://github.com/ImranR98/Obtainium/releases/download/v1.1.1/app-arm64-v8a-release.apk.sha256
+sha256sum -c app-arm64-v8a-release.apk.sha256
 
